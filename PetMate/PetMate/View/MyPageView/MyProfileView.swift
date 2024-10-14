b5045c3f5a9dab63c36f2f7613c9c6a8a1f94ef1
dardardardardar <180dayna@naver.com>
2024-10-14 23:25:40 +0900

    [Feat]: MyProfileView 추가

---
+++ PetMate/PetMate/View/MyPageView/MyProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  MyProfileView.swift
+//  PetMate
+//
+//  Created by 이다영 on 10/14/24.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyProfileView()
+}
