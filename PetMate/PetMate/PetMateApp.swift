9c674fa5b19cc5cc0616ee66d3d8de9df46bf1b5
gadisom <jeongwon.kim@gmail.com>
2024-10-14 15:46:10 +0900

    [Feat]: 초기 프로젝트 셋팅

---
+++ PetMate/PetMate/PetMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetMateApp.swift
+//  PetMate
+//
+//  Created by 김정원 on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PetMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
