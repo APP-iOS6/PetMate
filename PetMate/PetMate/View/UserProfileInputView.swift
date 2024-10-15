e8674c74a9b700752d6151bc8fbe57d3cab07101
Hyeonjeong-Sim <56778111@naver.com>
2024-10-15 23:19:56 +0900

    [feat]: UserProfileInputView(회원정보 입력 화면) 생성

---
+++ PetMate/PetMate/View/UserProfileInputView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileInputView.swift
+//  PetMate
+//
+//  Created by Hyeonjeong Sim on 10/15/24.
+//
+
+import SwiftUI
+
+struct UserProfileInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileInputView()
+}
