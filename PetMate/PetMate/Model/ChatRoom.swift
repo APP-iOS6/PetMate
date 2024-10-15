3e7d34261f5cfd3d0da0ce73c5961f0c10458020
kim dong kyung <kdkd422@naver.com>
2024-10-15 17:55:52 +0900

    Feat 채팅방 모델 추가

--- PetMate/PetMate/Model/ChatRoom.swift
+++ PetMate/PetMate/Model/ChatRoom.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct ChatRoom: Codable, Identifiable {
+    @DocumentID var id: String?
+    var postId: String? // 신청한 글의 ID
+    var participant: [String] // 대화 참가자들 UID (예: ["user1", "user2"])
+    var lastMessage: String
+    var lastMessageAt: Date
+    var readStatus: [String: Date] // 각 참가자의 마지막 읽은 메시지 시점
+}
+
+struct ChatRoomWithUser: Codable {
+    var chatRoom: ChatRoom
+    var chatUser: MateUser
+}
