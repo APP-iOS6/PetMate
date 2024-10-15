4fb95a060274e70649ec5a8c6df8a1e3d8063bfd
garden <jeongwon.kim64@gmail.com>
2024-10-16 03:27:46 +0900

    Revert "18 feat petmap"

--- PetMate/PetMate/App/PetMateApp.swift
+++ PetMate/PetMate/App/PetMateApp.swift
@@ -11,9 +11,7 @@ import FirebaseCore
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-
-      FirebaseApp.configure()
-
+    FirebaseApp.configure()
     return true
   }
 }
@@ -26,9 +24,7 @@ struct PetMateApp: App {
   var body: some Scene {
     WindowGroup {
       NavigationView {
-        //ContentView()
-          PetMapView()
-              .environment(PetPlacesStore())
+        ContentView()
       }
     }
   }
