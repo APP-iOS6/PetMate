cf441a7bf5f993c7b6811b8fb14362a22654ee98
Heecheol_K <hck1104@naver.com>
2024-10-15 23:29:01 +0900

    feat: 홈화면 뷰들 서브뷰로 나눔, ContentView에 탭뷰 만듬

---
+++ PetMate/PetMate/View/HomeView/HomeMainBannerView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeMainBannerView.swift
+//  PetMate
+//
+//  Created by 권희철 on 10/15/24.
+//
+
+import SwiftUI
+
+struct HomeMainBannerView: View {
+    var imageUrl: URL = URL(string:"https://www.dutch.com/cdn/shop/articles/shutterstock_1898629669.jpg?v=1697090094")!
+    var body: some View {
+        VStack{
+            AsyncImage(url: imageUrl){ image in
+                image.image?.resizable()
+                    .scaledToFit()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 200)
+        .border(.gray)
+    }
+}
+
+#Preview {
+    HomeMainBannerView()
+}
