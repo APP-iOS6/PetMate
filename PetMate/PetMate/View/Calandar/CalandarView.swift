87673b21c0000ae5f7da511ed4a8923cb02e549b
Heecheol_K <hck1104@naver.com>
2024-10-15 15:51:07 +0900

    feat: 캘린더 등록 시트 취소하는 기능 추가 #25

--- PetMate/PetMate/View/Calandar/CalandarView.swift
+++ PetMate/PetMate/View/Calandar/CalandarView.swift
@@ -23,20 +23,42 @@ struct CalandarView: View {
     }
 }
 
+//캘린더 뷰컨트롤러 뷰
 struct EventEditViewController: UIViewControllerRepresentable {
+    //캘린더 뷰는 외부에서 호출하기 때문에 presentationMode의 dismiss를 통해 시트를 내려야 함
+    @Environment(\.presentationMode) var presentationMode
+    private let store = EKEventStore()
+    let testEvent: TestEvent
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    //Delegate같은 VC의 대리자의 역할을 하기 위해 필요함
+    class Coordinator: NSObject, EKEventEditViewDelegate {
+        var parent: EventEditViewController
+        
+        init(_ controller: EventEditViewController) {
+            self.parent = controller
+        }
+        
+        func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith action: EKEventEditViewAction) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
     typealias UIViewControllerType = EKEventEditViewController
+    //EKEventEditViewController를 반환함
     func makeUIViewController(context: Context) -> EKEventEditViewController {
         let eventEditViewController =  EKEventEditViewController ()
         eventEditViewController.event = event
         eventEditViewController.eventStore = store
+        eventEditViewController.editViewDelegate = context.coordinator
         return eventEditViewController
     }
     
     func updateUIViewController(_ uiViewController: EKEventEditViewController, context: Context) {}
     
-    private let store = EKEventStore()
-    
-    let testEvent: TestEvent
+    //EKEventEditViewController를 띄웠을 때 폼의 값들을 미리 지정해줌
     private var event: EKEvent {
         let event = EKEvent(eventStore: store)
         event.title = testEvent.name
@@ -59,14 +81,14 @@ struct EventEditViewController: UIViewControllerRepresentable {
     }
 }
 
-
+//테스트용 모델
 struct TestEvent: Identifiable{
     var id = UUID()
     var name: String
     var startDate: Date
     var endDate: Date
     var description: String
-    
+    //이벤트 폼에서 날짜 시간을 지정하기 위해 Date타입을 쪼개주는 연산프로퍼티
     var startDateElements: (year: Int, month: Int, day: Int, hour: Int, minute: Int)? {
         let components = startDate.get(.day, .month, .year, .hour, .minute)
         if let year = components.year,
@@ -89,9 +111,9 @@ struct TestEvent: Identifiable{
         }
         return nil
     }
-   
+    
 }
-
+//이벤트 폼에서 날짜 시간을 지정하기 위해 Date타입을 쪼개주는 연산프로퍼티
 extension Date {
     func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
         return calendar.dateComponents(Set(components), from: self)
