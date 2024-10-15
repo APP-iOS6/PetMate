4dd235247f0b31e9dd3a49650530d10fe61cd773
kim dong kyung <kdkd422@naver.com>
2024-10-15 23:57:20 +0900

    Feat 채팅방 리스트네비게이션 링크 추가

--- PetMate/PetMate/View/ChatRoom/ChatRoomListView.swift
+++ PetMate/PetMate/View/ChatRoom/ChatRoomListView.swift
@@ -13,20 +13,22 @@ struct ChatRoomListView: View {
     private var viewModel: ChatRoomListViewModel = .init()
     
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 22) {
-                ForEach(viewModel.chatListRoom, id: \.chatRoom.id) { room in
-                    ChatRoomCellView(chatRoom: room)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 22) {
+                    ForEach(viewModel.chatListRoom, id: \.chatRoom.id) { room in
+                        ChatRoomCellView(chatRoom: room)
+                    }
+                    
+                    Button {
+                        viewModel.createChatRoom()
+                    } label: {
+                        Text("채팅 추가하기")
+                    }
+                    
                 }
-                
-                Button {
-                    viewModel.createChatRoom()
-                } label: {
-                    Text("채팅 추가하기")
-                }
-                
+                .padding()
             }
-            .padding()
         }
     }
 }
@@ -36,41 +38,44 @@ struct ChatRoomCellView: View {
     let chatRoom: ChatRoomWithUser
     
     var body: some View {
-        HStack {
-            AsyncImage(url: URL(string: chatRoom.chatUser.image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(1, contentMode: .fill)
-                    .frame(width: 60, height: 60)
-                    .clipShape(RoundedRectangle(cornerRadius: 24))
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 60, height: 60)
-            VStack {
-                HStack {
-                    Text(chatRoom.chatUser.name)
-                        .font(.headline)
-                    Spacer()
-                    Text(chatRoom.chatRoom.lastMessageAt.formattedTimeAgo)
-                        .frame(alignment: .topTrailing)
-                        .foregroundStyle(.secondary)
-                        .font(.caption)
+        NavigationLink {
+            Text("test")
+        } label: {
+            HStack {
+                AsyncImage(url: URL(string: chatRoom.chatUser.image)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fill)
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                } placeholder: {
+                    ProgressView()
                 }
-                .padding(.bottom, 4)
-               
-                
-                HStack {
-                    Text(chatRoom.chatRoom.lastMessage)
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
-                        .lineLimit(2)
+                .frame(width: 60, height: 60)
+                VStack {
+                    HStack {
+                        Text(chatRoom.chatUser.name)
+                            .foregroundStyle(.basic)
+                            .font(.headline)
+                        Spacer()
+                        Text(chatRoom.chatRoom.lastMessageAt.formattedTimeAgo)
+                            .frame(alignment: .topTrailing)
+                            .foregroundStyle(Color(uiColor: .systemGray2))
+                            .font(.caption)
+                    }
+                    .padding(.bottom, 4)
                     
-                    Spacer()
+                    HStack {
+                        Text(chatRoom.chatRoom.lastMessage)
+                            .font(.caption)
+                            .foregroundStyle(Color(uiColor: .systemGray2))
+                            .lineLimit(2)
+                        
+                        Spacer()
+                    }
                 }
             }
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 #Preview {
