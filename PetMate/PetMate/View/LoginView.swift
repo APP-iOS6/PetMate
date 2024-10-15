2a3ffb1b8cf3cec49f0738086816b5a9fe399b8d
Hyeonjeong-Sim <56778111@naver.com>
2024-10-15 11:32:00 +0900

    [feat]: 로그인 UI 레이아웃 추가 및 버튼 이미지 적용

--- PetMate/PetMate/View/LoginView.swift
+++ PetMate/PetMate/View/LoginView.swift
@@ -9,10 +9,67 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Spacer()
+
+            // 로고 이미지
+            Image("")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 100)
+            
+            // 온보딩 이미지
+            Image("")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 100)
+            
+            // 카카오 로그인
+            Button(action: {
+                // 카카오 로그인 동작 추가
+            }) {
+                Image("kakao_login_button")
+                    .resizable()
+                    .frame(width: 300, height: 60)
+            }
+
+            // 애플 로그인
+            Button(action: {
+                // 애플 로그인 동작 추가
+            }) {
+                Image("apple_login_button")
+                    .resizable()
+                    .frame(width: 300, height: 60)
+            }
+
+            // 구글 로그인 버튼 이미지
+            Button(action: {
+                // 구글 로그인 동작 추가
+            }) {
+                Image("google_login_button")
+                    .resizable()
+                    .frame(width: 300, height: 60)
+            }
+
+            // 둘러보기 버튼
+            Button(action: {
+                // 둘러보기 동작 추가
+            }) {
+                Text("로그인 전 둘러보기")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .underline(true, color: .gray)
+                    .foregroundColor(.gray)
+            }
+
+            Spacer()
+        }
+        .padding()
     }
 }
 
-#Preview {
-    LoginView()
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
