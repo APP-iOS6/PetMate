2719ba848ff9900f7cbda271e4bfb5887836ed4b
dardardardardar <180dayna@naver.com>
2024-10-15 14:55:34 +0900

    [Feat]: ImagePicker파일 생성

--- PetMate/PetMate/View/MyPageView/UserProfileView.swift
+++ PetMate/PetMate/View/MyPageView/UserProfileView.swift
@@ -81,40 +81,6 @@ struct ProfileView: View {
     }
 }
 
-// 이미지 선택기
-struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: Image?
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = .photoLibrary
-        return picker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        
-        init(_ parent: ImagePicker) {
-            self.parent = parent
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let uiImage = info[.originalImage] as? UIImage {
-                parent.image = Image(uiImage: uiImage)
-            }
-            picker.dismiss(animated: true)
-        }
-    }
-}
-
-
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
