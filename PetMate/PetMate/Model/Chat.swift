276efef200ebfbb816f86696484168dd38ecd120
kim dong kyung <kdkd422@naver.com>
2024-10-15 23:55:52 +0900

    Feat 챗 모델 생성

--- PetMate/PetMate/Model/Chat.swift
+++ PetMate/PetMate/Model/Chat.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct Chat: Codable {
+    @DocumentID var id: String?
+    var message: String
+    var sender: String //보낸 사람의 uid
+    var readBy: [String] //1:1 대화이지만 읽은 사람들의 uid를 모아둠
+    var createAt: Date
+}
