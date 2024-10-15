e0f5ed27257e30979099b1cf93cbb995fe3ee355
garden <jeongwon.kim64@gmail.com>
2024-10-16 03:24:34 +0900

    Revert "36 feat main view"

--- PetMate/PetMate/View/ContentView.swift
+++ PetMate/PetMate/View/ContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Text("Home")
-                    Image(systemName: "house")
-                }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-        
+        .padding()
     }
 }
 
