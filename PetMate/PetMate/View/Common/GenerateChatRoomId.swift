d1cc9b62e7338ddbba275fea9cef3ba9e860edd3
kim dong kyung <kdkd422@naver.com>
2024-10-16 22:50:21 +0900

    Feat 채팅방 문서 ID 생성 함수 추가

--- PetMate/PetMate/View/Common/GenerateChatRoomId.swift
+++ PetMate/PetMate/View/Common/GenerateChatRoomId.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+
+func generateChatRoomId(userId1: String, userId2: String) -> String {
+    return userId1 < userId2 ? "\(userId1)_\(userId2)" : "\(userId2)_\(userId1)"
+}
