a3495424475b7dec3599fee9237d26fcb39e096d
kim dong kyung <kdkd422@naver.com>
2024-10-16 22:49:55 +0900

    Feat 텍스트필드 모디파이어 추가

--- PetMate/PetMate/View/Common/Modifier/TextFieldModifier.swift
+++ PetMate/PetMate/View/Common/Modifier/TextFieldModifier.swift
@@ -5,4 +5,17 @@
 //  Created by 김동경 on 10/16/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(4)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.loginText, lineWidth: 1)
+                    .foregroundStyle(.clear)
+                
+            }
+    }
+}
