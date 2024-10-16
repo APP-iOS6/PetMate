d3c7d5c11b1fc2099966a03da0939fb121ce572f
kim dong kyung <kdkd422@naver.com>
2024-10-16 22:47:21 +0900

    Feat 채팅 디테일뷰 UIKit 텍스트 뷰 및 키보드 처리 완료

--- PetMate/PetMate/View/ChatRoom/ChatDetailView.swift
+++ PetMate/PetMate/View/ChatRoom/ChatDetailView.swift
@@ -9,24 +9,92 @@ import SwiftUI
 
 struct ChatDetailView: View {
     
-    let chatWithUser: ChatRoomWithUser
+    @StateObject private var viewModel: ChatDetailViewModel = ChatDetailViewModel()
+    @State private var text: String = ""
+    @State private var height: CGFloat = 0
+    @State private var keyboardHeight: CGFloat = 0
+    
+    let otherUser: MateUser
+    let postId: String?
     
     init(
-        chatWithUser: ChatRoomWithUser
+        otherUser: MateUser,
+        postId: String? = nil
     ) {
-        self.chatWithUser = chatWithUser
+        self.otherUser = otherUser
+        self.postId = postId
     }
     
-    @StateObject private var viewModel: ChatDetailViewModel = ChatDetailViewModel()
-    
     var body: some View {
-        List {
-            ForEach(viewModel.chatList, id: \.id) { chat in
-                Text(chat.message)
+        VStack {
+            if viewModel.post != nil {
+                
             }
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.chatList, id: \.id) { chat in
+                        if chat.sender == otherUser.id {
+                            Text(chat.message)
+                                .bold()
+                                .foregroundStyle(.black)
+                        } else {
+                            Text(chat.message)
+                                .bold()
+                                .foregroundStyle(.yellow)
+                        }
+                    }
+                }
+            }
+            .frame(maxHeight: .infinity)
+            
+            Spacer()
+            
+            
+            HStack {
+                Button {
+                    
+                    
+                } label: {
+                    Image(systemName: "plus")
+                        .bold()
+                        .tint(.loginText)
+                }
+                
+                ResizableTextField(text: self.$text, height: self.$height)
+                    .frame(height: self.height < 150 ? self.height : 150)
+                    .modifier(TextFieldModifier())
+                    .overlay(alignment: .leading) {
+                        if text.isEmpty {
+                            Text("메시지를 입력해 주세요.")
+                                .transition(.opacity)
+                                .offset(x: 8)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                
+                Button {
+                    self.text = ""
+                } label: {
+                    Image(systemName: "paperplane.circle")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .tint(.loginText)
+                }
+            }
+            .animation(.smooth, value: text)
+            .padding(.horizontal)
+            
+        }
+        .padding(.bottom, 15)
+        .onTapGesture {
+            UIApplication.shared.endEditing()
         }
         .onAppear {
-            viewModel.observeChatList(chatWithUser)
+            viewModel.checkChatRoom(otherUser.id ?? "")
+            // 키보드가 내려갈 때 높이를 0으로 설정
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardDidHideNotification, object: nil, queue: .main) { _ in
+                self.keyboardHeight = 0
+            }
         }
         .onDisappear {
             viewModel.listener?.remove()
@@ -34,3 +102,9 @@ struct ChatDetailView: View {
     }
 }
 
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
