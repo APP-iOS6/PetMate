7468a2b0a2bc6d02986ded545079819de771660a
kim dong kyung <kdkd422@naver.com>
2024-10-18 22:19:46 +0900

    Feat 회원가입 주소 기능 추가중

--- PetMate/PetMate/View/SignUp/SignUpAddressView.swift
+++ PetMate/PetMate/View/SignUp/SignUpAddressView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct SignUpAddressView: View {
+    
+    var viewModel: SignUpViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            VStack(alignment: .leading) {
+                Text("나의 동네를 입력해 주세요.")
+                    .bold()
+                    .font(.title2)
+                
+                Spacer()
+                    .frame(maxHeight: proxy.size.height * 0.15)
+                
+                let locationEmpty: Bool = viewModel.mateUser.location.isEmpty
+                HStack {
+                    Text(locationEmpty ? "서울시 마포구" : viewModel.mateUser.location)
+                        .frame(maxWidth: proxy.size.width * 0.7, alignment: .leading)
+                        .foregroundStyle(locationEmpty ? .secondary : .primary)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("검색")
+                            .padding(.horizontal)
+                            .padding(.vertical, 6)
+                            .foregroundStyle(Color.accentColor)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 3)
+                                    .stroke(Color.accentColor, lineWidth: 1)
+                            }
+                    }
+                        
+                }
+                
+                Divider()
+                    .frame(maxWidth: proxy.size.width * 0.7, alignment: .leading)
+            }
+            .padding()
+        }
     }
 }
 
 #Preview {
-    SignUpAddressView()
+    SignUpAddressView(viewModel: SignUpViewModel())
+        .padding()
 }
