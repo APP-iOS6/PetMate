733fa269395817580cac10fc03f0d589cf498158
kim dong kyung <kdkd422@naver.com>
2024-10-18 22:19:10 +0900

    chore project.pbxproj

---
+++ PetMate/PetMate/View/SignUp/SignUpAddressView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpAddressView.swift
+//  PetMate
+//
+//  Created by 김동경 on 10/18/24.
+//
+
+import SwiftUI
+
+struct SignUpAddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpAddressView()
+}
