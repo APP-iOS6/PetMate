58dc1fe6955326d79b55dbab3ea7edef9f94404d
kim dong kyung <kdkd422@naver.com>
2024-10-18 22:20:28 +0900

    Feat 회원가입 뷰모델

--- PetMate/PetMate/View/SignUp/SignUpViewModel.swift
+++ PetMate/PetMate/View/SignUp/SignUpViewModel.swift
@@ -13,4 +13,11 @@ import Observation
 @Observable
 class SignUpViewModel {
     
+    enum Progress: Double {
+        case address = 0.5
+        case profile = 1.0
+    }
+    
+    var progress: Progress = .address
+    var mateUser: MateUser = MateUser(id: nil, name: "", image: "", matchCount: 0, location: "", createdAt: Date())
 }
