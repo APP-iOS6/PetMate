b09fa0df93d01ec9cb84453960c721eae3624a70
kim dong kyung <kdkd422@naver.com>
2024-10-18 22:20:43 +0900

    Feat 회원가입 컨테이너 뷰

--- PetMate/PetMate/View/SignUp/SignUpContainerView.swift
+++ PetMate/PetMate/View/SignUp/SignUpContainerView.swift
@@ -12,7 +12,45 @@ struct SignUpContainerView: View {
     private var viewModel: SignUpViewModel = .init()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                
+                TopHeader()
+                
+                ProgressView(value: viewModel.progress.rawValue, total: 1.0)
+                    .tint(Color.accentColor)
+                    .progressViewStyle(.linear)
+                    .padding(.bottom, 24)
+                    .animation(.easeInOut(duration: 0.1), value: viewModel.progress)
+                
+                switch viewModel.progress {
+                case .address:
+                    SignUpAddressView(viewModel: viewModel)
+                case .profile:
+                    EmptyView()
+                }
+            }
+            .padding()
+        }
+    }
+    
+    private func TopHeader() -> some View {
+        HStack {
+            Text("사용자 정보 입력하기")
+                .font(.title2)
+        }
+        .frame(maxWidth: .infinity)
+        .overlay(alignment: .leading) {
+            Button {
+                goBack()
+            } label: {
+                Image(systemName: "chevron.left")
+            }
+        }
+    }
+    
+    private func goBack() {
+        
     }
 }
 
