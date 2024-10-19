2055d2a7d31bf23e7c55a589e839888b8024b5ec
kim dong kyung <kdkd422@naver.com>
2024-10-19 17:44:28 +0900

    Feat 회원가입 후 웰컴 뷰 추가

--- PetMate/PetMate/View/Welcome/WelcomeView.swift
+++ PetMate/PetMate/View/Welcome/WelcomeView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    @Environment(AuthManager.self) var authManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.welcome)
+                .resizable()
+                .aspectRatio(1, contentMode: .fill)
+                .frame(width: .screenWidth * 0.6, height: .screenWidth * 0.6)
+            
+            Text("회원 가입이 완료 되었습니다!")
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.welcome)
+                .padding(.vertical)
+            
+            Button {
+                
+            } label: {
+                Text("+ 펫 정보 입력하기")
+                    .padding()
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(Color.accentColor)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.accentColor, lineWidth: 1)
+                    }
+                
+            }
+            .padding(.vertical, 12)
+            
+            Button {
+                authManager.authState = .auth
+            } label: {
+                Text("PetMate 시작하기")
+                    .modifier(ButtonModifier())
+                    .bold()
+            }
+        }
+        .padding(.horizontal, 32)
     }
 }
 
 #Preview {
     WelcomeView()
+        .environment(AuthManager())
 }
