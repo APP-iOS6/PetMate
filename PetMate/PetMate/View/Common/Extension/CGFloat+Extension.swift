6aa6d33ddeab06ff3a96856b96540dc958de90e6
kim dong kyung <kdkd422@naver.com>
2024-10-19 17:41:25 +0900

    Feat UIScreen 사이즈 익스텐션 추가

--- PetMate/PetMate/View/Common/Extension/CGFloat+Extension.swift
+++ PetMate/PetMate/View/Common/Extension/CGFloat+Extension.swift
@@ -5,4 +5,9 @@
 //  Created by 김동경 on 10/19/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
