3fdb7817a3fbe6581420f004727d1ad19a44f18d
kim dong kyung <kdkd422@naver.com>
2024-10-21 00:20:21 +0900

    Feat 태그 모디파이어 추가

--- PetMate/PetMate/View/Common/Modifier/TagModifier.swift
+++ PetMate/PetMate/View/Common/Modifier/TagModifier.swift
@@ -5,4 +5,21 @@
 //  Created by 김동경 on 10/20/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TagModifier: ViewModifier {
+    
+    let selected: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .bold()
+            .font(.caption)
+            .padding(.horizontal, 8)
+            .padding(.vertical)
+            .background(selected ? .petTag : Color(uiColor: .systemGray6))
+            .foregroundStyle(selected ? .white : .petTag)
+            .clipShape(.rect(cornerRadius: 32))
+    }
+}
+
