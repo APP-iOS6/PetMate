19c41cd7835692d3c41e94cbd97bb8c385f78e3b
kim dong kyung <kdkd422@naver.com>
2024-10-23 04:58:18 +0900

    Feat 모디파이어 추가

--- PetMate/PetMate/View/Common/Modifier/ComfirmMateModifier.swift
+++ PetMate/PetMate/View/Common/Modifier/ComfirmMateModifier.swift
@@ -5,4 +5,21 @@
 //  Created by 김동경 on 10/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ComfirmMateModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 14))
+            .bold()
+            .foregroundStyle(.petTag)
+            .padding(.horizontal)
+            .padding(.vertical, 8)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color(uiColor: .systemGray6), lineWidth: 1)
+            }
+            .padding(.trailing, 12)
+    }
+}
