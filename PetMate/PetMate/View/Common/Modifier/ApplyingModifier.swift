dea6fad7e77ac6bfdb6765a1ad2982cd9c534d8b
kim dong kyung <kdkd422@naver.com>
2024-10-23 04:57:56 +0900

    Feat 메이트확정하기 모디파이어

--- PetMate/PetMate/View/Common/Modifier/ApplyingModifier.swift
+++ PetMate/PetMate/View/Common/Modifier/ApplyingModifier.swift
@@ -5,4 +5,22 @@
 //  Created by 김동경 on 10/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ApplyingModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 14))
+            .font(.body)
+            .bold()
+            .padding(.horizontal)
+            .padding(.vertical, 8)
+            .foregroundStyle(.petTag)
+            .overlay {
+                RoundedRectangle(cornerRadius: 24)
+                    .stroke(Color(uiColor: .systemGray6), lineWidth: 1)
+            }
+            .padding(.trailing, 24)
+    }
+}
