6dbcff28df7548915aaf20bbc5560d0b97792c7e
gadisom <jeongwon.kim@gmail.com>
2024-10-24 03:17:01 +0900

    Feat: 리뷰 뷰 추가

--- PetMate/PetMate/View/Place/ReviewView.swift
+++ PetMate/PetMate/View/Place/ReviewView.swift
@@ -10,23 +10,29 @@ import FirebaseFirestore
 
 // 리뷰 뷰
 struct ReviewView: View {
-    var review: Review
+
+    var placePost: PlacePost
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             HStack {
-                // 프로필 이미지 (둥글게 처리)
-                Image(systemName: "person.circle.fill") //이미지 넣어야함
+                Image("dog")
                     .resizable()
-                    .frame(width: 40, height: 40)
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
                     .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color(.systemGray2), lineWidth: 1)
+                    )
+                    .padding()
                 
                 VStack(alignment: .leading) {
-                    Text("작성자 이름") // 이름 넣어야함
+                    Text("가디") // 이름 넣어야함
                         .font(.subheadline)
                         .bold()
                     
-                    Text("구경 3등") // 동 넣어야함
+                    Text("역삼동") // 동 넣어야함
                         .font(.caption)
                         .foregroundColor(.gray)
                 }
@@ -35,30 +41,22 @@ struct ReviewView: View {
             
             Divider()
             
-            Text(review.content)
-                .font(.body)
-                .padding(.bottom, 4)
-            
-            HStack {
-                Text("\(review.rating)개의 발점") //발점 넣어야함
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-                
-                Spacer()
-                
-                Text("어제 다녀왔어요") // 날짜 넣어야함
-                    .font(.caption)
-                    .foregroundColor(.gray)
+            VStack {
+                Text(placePost.title)
+                    .font(.title3)
+                    .bold()
+                    .padding(.bottom, 4)
+                Text(placePost.content)
+                    .padding(.bottom)
             }
+            .padding(.horizontal)
+            
         }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(12)
-        .overlay(
-            RoundedRectangle(cornerRadius: 12)
-                .stroke(Color.gray, lineWidth: 1)
-        )
-        .padding(.horizontal) // 스토어랑 좌우 패딩값이 다름 
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.white)
+                .stroke(Color.secondary, lineWidth: 0.5)
+        }
     }
 }
 
--- PetMate/PetMate/View/Place/StoreDetailView.swift
+++ PetMate/PetMate/View/Place/StoreDetailView.swift
@@ -13,93 +13,97 @@ struct StoreDetailView: View {
     var placePost: PlacePost
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Spacer()
-                AsyncImage(url: URL(string: placePost.image)) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 200)
-                        .clipped()
-                        .cornerRadius(12)
-                } placeholder: {
-                    ProgressView()
-                        .frame(height: 200)
-                }
-                Spacer()
-            }
-            
-            VStack(alignment: .leading, spacing: 10) {
+        ScrollView {
+            VStack(alignment: .leading) {
                 HStack {
-                    Text("📍\(placePost.placeName)")
-                        .font(.title3)
-                        .bold()
-                        .padding(.vertical, 8)
-                        .lineLimit(1)
-                    
                     Spacer()
+                    AsyncImage(url: URL(string: placePost.image)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 200)
+                            .clipped()
+                            .cornerRadius(12)
+                    } placeholder: {
+                        ProgressView()
+                            .frame(height: 200)
+                    }
+                    Spacer()
+                }
+                .padding(.top)
+                
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Text("📍\(placePost.placeName)")
+                            .font(.title3)
+                            .bold()
+                            .padding(.vertical, 8)
+                            .lineLimit(1)
+                        
+                        Spacer()
+                        
+                        Text("반려동물 동반")
+                            .font(.caption)
+                            .foregroundColor(.red)
+                            .padding(6)
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(12)
+                    }
                     
-                    Text("반려동물 동반")
-                        .font(.caption)
-                        .foregroundColor(.red)
-                        .padding(6)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(12)
-                }
-                
-                HStack(spacing: 6) {
-                    Image(systemName: "mappin.and.ellipse")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                    Text("\(placePost.address)")
-                }
-                .font(.subheadline)
-                .padding(.horizontal, 5)
-                
-                HStack(spacing: 6) {
-                    Image(systemName: "fork.knife")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                    Text(placePost.category)
-                }
-                .font(.footnote)
-                .padding(.horizontal, 5)
-                if let phone = placePost.phone {
                     HStack(spacing: 6) {
-                        Image(systemName: "phone.fill")
+                        Image(systemName: "mappin.and.ellipse")
                             .resizable()
                             .frame(width: 15, height: 15)
-                        Text(phone)
+                        Text("\(placePost.address)")
+                    }
+                    .font(.subheadline)
+                    
+                    HStack(spacing: 6) {
+                        Image(systemName: "fork.knife")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(placePost.category)
                     }
                     .font(.footnote)
-                    .padding(.horizontal, 5)
+                    
+                    if let phone = placePost.phone {
+                        HStack(spacing: 6) {
+                            Image(systemName: "phone.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
+                            Text(phone)
+                        }
+                        .font(.footnote)
+                        .padding(.horizontal, 5)
+                    }
+                    HStack(spacing: 6) {
+                        Image(systemName: "car.fill")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(placePost.isParking ? "주차 가능" : "주차 불가")
+                    }
+                    .font(.footnote)
                 }
-                HStack(spacing: 6) {
-                    Image(systemName: "car.fill")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                    Text(placePost.isParking ? "주차 가능" : "주차 불가")
-                }
-                .font(.footnote)
-                .padding(.horizontal, 5)
+                .padding()
             }
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.white)
+                    .stroke(Color.secondary, lineWidth: 0.5)
+            }
+            .padding(.vertical)
+            ReviewView(placePost: placePost)
         }
-        .padding()
-        .background(
-            RoundedRectangle(cornerRadius: 16)
-                .fill(Color.white)
-                .shadow(color: Color.black.opacity(0.1), radius: 4)
-        )
-        .padding()
+        .padding(.horizontal)
     }
 }
-//
-//// 프리뷰에 더미 데이터 적용
-//struct StoreDetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        let dummyPlacePost = PlacePost(id: "1", writeUser: "김정원", title: "카페 후기", content: "맛집이에요", location: GeoPoint(latitude: 37.5665, longitude: 126.9780), image: "", address: "경기도 수원시 매탄동 393", placeName: "카카오프렌즈 코엑스점", isParking: true, createdAt: Date(), updatedAt: Date())
-//        
-//        StoreDetailView(placePost: dummyPlacePost)
-//    }
-//}
+
+// 프리뷰에 더미 데이터 적용
+struct StoreDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let dummyPlacePost = PlacePost(id: "1", writeUser: "김정원", title: "카페 후기", content: "맛집이에요", location: GeoPoint(latitude: 37.5665, longitude: 126.9780), image: "", address: "경기도 수원시 매탄동 393", placeName: "카카오프렌즈 코엑스점", category: "", isParking: true, createdAt: Date(), updatedAt: Date())
+        
+        StoreDetailView(placePost: dummyPlacePost)
+    }
+}
